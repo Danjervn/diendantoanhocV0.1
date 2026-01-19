31569cc0257926eac73c5ae2f68c346cf09c6681
Danjervn <danjervn@gmail.com>
2026-01-19 11:15:34 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// Thay giá trị dưới bằng Supabase project values của bạn
+window.SUPABASE_URL = "https://elcdbohdymtrkcbmubfe.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_5OERPh4JAAC-dxkJQ6VcKA_p1E0LoSC";
